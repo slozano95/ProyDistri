ad38726eed177116065ed4276098a31c8c836b75
alejandrachacon <alejandra.kcm@gmail.com>
2017-09-25 22:00:54 -0500

    Last version

---
+++ src/Partida.java
@@ -0,0 +1,21 @@
+
+import java.util.Date;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ale
+ */
+public class Partida {
+    
+    private List<Equipo> equipos;
+    private Date duracion;
+    private int id;
+    
+}
