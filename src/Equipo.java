e640837e0d0d07c280ebebf8e9153189f54b99fe
alejandrachacon <alejandra.kcm@gmail.com>
2017-09-25 21:56:28 -0500

    Last version

---
+++ src/Equipo.java
@@ -0,0 +1,52 @@
+
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ale
+ */
+public class Equipo {
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Jugador> getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(List<Jugador> jugadores) {
+        this.jugadores = jugadores;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public Equipo(String nombre) {
+        this.nombre = nombre;
+        jugadores = new ArrayList<Jugador>();
+        puntos = 0;
+    }
+    
+    private String nombre;
+    private List<Jugador> jugadores;
+    private int puntos;
+    
+    
+    
+}
